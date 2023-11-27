f6280b8f122205cc9e6cbf2655c8b402cfec94e6
kamruzzamanripon <rkamruzzaman@gmail.com>
2023-11-28 03:20:43 +0600

    minro css add

--- app/ai-make-movie/loading.js
+++ app/ai-make-movie/loading.js
@@ -3,7 +3,7 @@ import Image from 'next/image';
 const loading = () => {
     return (
         <div className="flex items-center justify-center h-screen">
-            <h3 className="font-semibold text-3xl text-red-500 absolute top-5 left-6 absolute top-3/4 left-[28%]">
+            <h3 className="font-semibold text-3xl text-red-500 absolute top-3/4 left-[28%]">
                 wait....., I am going to the director to make this movie.
             </h3>
             
