ad38721e47946f929c81999758b1874298d51d14
kamruzzamanripon <rkamruzzaman@gmail.com>
2023-11-29 02:25:58 +0600

    css fix

--- app/component/index/CategoryWiseMovies.js
+++ app/component/index/CategoryWiseMovies.js
@@ -38,7 +38,13 @@ const CategoryWiseMovies = () => {
           <div className="mt-4 grid grid-cols-2 gap-y-5 sm:grid-cols-3 gap-x-5 ">
             {category.movies.map((movie, indexTwo) => (
               <div className="flex flex-col rounded-xl overflow-hidden aspect-square border dark:border-zinc-600" key={indexTwo}>
-                
+                <div className="w-full h-1/5 bg-white dark:bg-zinc-800 dark:text-white px-3 flex items-center justify-between border-t-2 border-t-red-600">
+                  <span className="capitalize  font-medium truncate">{movie.title}</span>
+                  <div className="flex space-x-2 items-center text-xs">
+                    <ImdbLogo />
+                    <span>{getRandomNumber()}</span>
+                  </div>
+                </div>
                 <Image
                   src={movie.image}
                   className=" h-4/5 object-cover w-full"
@@ -47,13 +53,6 @@ const CategoryWiseMovies = () => {
                 height={105} 
                 layout="responsive"
                 />
-                <div className="w-full h-1/5 bg-white dark:bg-zinc-800 dark:text-white px-3 flex items-center justify-between border-t-2 border-t-red-600">
-                  <span className="capitalize  font-medium truncate">{movie.title}</span>
-                  <div className="flex space-x-2 items-center text-xs">
-                    <ImdbLogo />
-                    <span>{getRandomNumber()}</span>
-                  </div>
-                </div>
               </div>
             ))}
 
